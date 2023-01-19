d052b702e296b333a961b2e889f89d50ab79f208
Will Burnton <willburnton@Wills-MacBook-Air.local>
2023-01-19 17:08:29 -0600

    file move

--- server.js
+++ server.js
@@ -17,5 +17,8 @@ app.get("/", function (req, res) {
 
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
-}) 
+}); 
 
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname,'index.html'));
+});
