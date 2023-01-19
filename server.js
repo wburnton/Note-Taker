3404ea211da5fefc6fd7c51c56b8a531858ab0b6
Will Burnton <willburnton@Wills-MacBook-Air.local>
2023-01-18 18:38:49 -0600

    server js file

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const database = require("./db/db")  
+
+var app = express();
+var PORT = process.env.PORT || 3000; 
+
+app.use(express.static('public')); 
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); 
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+}); 
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+}) 
+
