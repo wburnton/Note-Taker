1e2b980b4391166f2cf83083187a63cad49dd73c
Will Burnton <willburnton@Wills-MacBook-Air.local>
2023-01-19 21:32:10 -0600

    route layout

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const fs = require("fs");
-const path = require("path");
-const database = require("./db/db")  
+const path = require("path");  
+const database = require('./db/db.json'); 
+const uuid = require("uuid");
 
 var app = express();
 var PORT = process.env.PORT || 3000; 
@@ -21,4 +22,39 @@ app.get("/notes", function (req, res) {
 
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname,'index.html'));
-});
+}); 
+
+app.get('/api/notes', function (req, res) {
+    fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, data) {
+      if (error) {
+        return console.log(error)
+      }
+      data = JSON.parse(data)
+  
+      return res.json(data)
+    })
+  }); 
+
+  app.post("/api/notes", function (req, res) {
+    let allNotes = [];
+    let newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuid.v4(),
+    }
+    fs.readFile(__dirname + "/../db/db.json", (err, data) => {
+        if (err) throw err;
+        allNotes = JSON.parse(data);
+        allNotes.push(newNote);
+        fs.writeFile(__dirname + "/../db/db.json", JSON.stringify(allNotes), "utf-8", (err) => {
+            if (err) throw err;
+            console.log("The note has been saved.")
+            res.end();
+        })
+    })
+    console.log(newNote)
+}); 
+
+app.listen(PORT, function () {
+    console.log(`App is listening on PORT: ${PORT}`);
+  });
